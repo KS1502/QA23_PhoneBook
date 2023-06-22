77cf7682414c6b2260065ace66dcb7d1c6764837
Kira Schmidt <kirakrg@mail.ru>
2023-06-22 15:49:46 +0200

    PhoneBook.Tests

---
+++ PhoneBook.Tests/src/test/java/tests/HomePageTests.java
@@ -0,0 +1,17 @@
+package tests;
+
+//package com.phonebook.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class HomePageTests extends TestBase {
+
+        @Test
+        public void openHomePageTest() {
+
+            Assert.assertTrue(app.getHomePage().isHomeComponentPresent());
+        }
+
+    }
